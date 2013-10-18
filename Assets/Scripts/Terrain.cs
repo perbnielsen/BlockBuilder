fa81881a51c065cc30ea973246f78d78589b2c3e
Per Bleshøy Nielsen <per.b.nielsen@gmail.com>
2013-10-18 08:39:17 +0800

    Fixed noise to actually generate interesting landscape Let F1/F2 change viewDistance

--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -8,12 +8,24 @@ public class Terrain : MonoBehaviour
 	public Transform player;
 	public int seed;
 	public int chunkSize;
-	//	public float preloadChunksDistance;
-	public float displayChunkDistance;
-	//	public float destroyChunkDistance;
-	[NonSerializedAttribute]
+	float _displayChunkDistance;
+
+
+	public float displayChunkDistance
+	{
+		get { return _displayChunkDistance; }
+		set
+		{
+			_displayChunkDistance = value;
+			displayChunkDistanceSqr = displayChunkDistance * displayChunkDistance;
+			destroyChunkDistanceSqr = (displayChunkDistance + chunkSize) * (displayChunkDistance + chunkSize);
+		}
+	}
+
+
+	[NonSerialized]
 	public float displayChunkDistanceSqr;
-	[NonSerializedAttribute]
+	[NonSerialized]
 	public float destroyChunkDistanceSqr;
 	public Dictionary< Position3, Chunk > chunks = new Dictionary< Position3, Chunk >();
 	public Chunk chunkPrefab;
@@ -44,14 +56,16 @@ public class Terrain : MonoBehaviour
 	{
 //		UnityEnigne.Random.seed = seed;
 
-		displayChunkDistanceSqr = displayChunkDistance * displayChunkDistance;
-		destroyChunkDistanceSqr = (displayChunkDistance + chunkSize) * (displayChunkDistance + chunkSize);
+		displayChunkDistance = 128;
 
 	}
 
 
 	void Update()
 	{
+		if ( Input.GetKey( KeyCode.F1 ) ) displayChunkDistance -= 8;
+		if ( Input.GetKey( KeyCode.F2 ) ) displayChunkDistance += 8;
+
 		getChunk( player.transform.position / chunkSize );
 	}
 }
